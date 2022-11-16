814d7091a4c0e0170aee9d56e018e71eacd70a7c
UrgyenTsering <urgentsering77@gmail.com>
2022-11-16 22:26:29 +0545

    update

--- Server/src/Controller/registerRouter.js
+++ Server/src/Controller/registerRouter.js
@@ -1,48 +1,10 @@
 const express = require("express");
-<<<<<<< HEAD
 const usersModel = require("../Model/usersSchema");
 const router = express.Router();
-
-
 // post request for register the user and all the router should be in /
 router.post("/", async (req, res) => {
     usersModel.create(req.body)
     console.log(req.body)
-=======
-const User = require('../Model/usersSchema')
-const router = express.Router();
-
-
-// post request for register the user
-router.post("/register", async (req, res) => {
-    try{
-        console.log(req.body)
-        const appUser = await User.create(req.body)
-       
-    }catch(error){
-        console.log(error)
-    }
-});
-
-router.post("/login", async (req, res) => {
-    try{
-        console.log(req.body)
-        if(req.body.phoneNmber && req.body.password){
-            const registeredUser = await User.findOne(req.body)
-            if(registeredUser){
-				res.send(registeredUser)
-			}else{
-				res.json({message: "No user found"})
-			}
-        }else{
-			res.json({message: "All fields are required. Complete the form!!"})
-		}
-        
-       
-    }catch(error){
-        console.log(error)
-    }
->>>>>>> 1f802ac609b7042d8065b377e0fce8cd928f566a
 });
 
 // view users
--- Server/src/Model/usersSchema.js
+++ Server/src/Model/usersSchema.js
@@ -2,22 +2,14 @@ const mongoose = require('mongoose')
 
 const usersSchema = new mongoose.Schema(
     {
-<<<<<<< HEAD
       firstName: { type: String },
       lastName: { type: String },
       phoneNumber: { type: String},
       address: { type: String },
       email: { type: String},//email should be unique
+      role:{ type: String},
       password: { type: String},
       confrimPassword: { type: String },
-=======
-      name: { type: String, required: true },
-      phoneNumber: { type: Number, unique: true },
-      address: { type: String, required: true },
-      email: { type: String, required: true },
-      password: { type: String, required: true },
-      confirmPassword: { type: String, required: true },
->>>>>>> 1f802ac609b7042d8065b377e0fce8cd928f566a
     },
     {
       collection: "Users",
