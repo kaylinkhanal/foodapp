193d6660e1e7963088bdce8dc139f7b60616cc0a
UrgyenTsering <urgentsering77@gmail.com>
2022-11-16 20:37:44 +0545

    updated

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,13 @@
-import Login from "./pages/Auth/Login";
+import React from "react";
 import { Routes, BrowserRouter as Router, Route, Link } from "react-router-dom";
-<<<<<<< HEAD
-import Register from "./pages/Auth/Register"
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 const App = () => {
   return (
     <div className="parent">
-=======
-import Register from "./pages/Auth/Register";
-import Header from "./component/header";
-
-const App = () => {
-  return (
-    <div className="App">
-      {/* <Header/> */}
->>>>>>> 1f802ac609b7042d8065b377e0fce8cd928f566a
       <Routes>
        <Route path="/register" element={<Register/>} />
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
