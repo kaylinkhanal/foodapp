1338b3794023f71333181a294b14ee5d0571ff99
medanieldark <medanieldark@gmail.com>
2022-11-18 07:25:08 +0545

    login change

--- Server/src/Controller/loginRouter.js
+++ Server/src/Controller/loginRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/", async (req, res) => {
     try{
         console.log(req.body)
-        if(req.body.phoneNmber && req.body.password){
+        if(req.body.email && req.body.password){
             const registeredUser = await User.findOne(req.body)
             if(registeredUser){
 				res.send(registeredUser)
