f31a427c26d24e65df4ae5f8a968ce202b478fbe
Ambica <ambicana5@gmail.com>
2022-11-22 07:44:32 +0545

    added header component

--- Client/src/component/header/header.js
+++ Client/src/component/header/header.js
@@ -1,13 +1,23 @@
 import React from "react";
-import Logo from '../../images/foodapp.png'
+import Logo from '../../images/meal.png'
+import './header.css'
 
 const Header = ()=>{
     return(
         <>
             <header>
                 <div className="container">
-                    <div className="logo">
-                        <img src={Logo} alt="Logo"></img>
+                    <div className="header_wrap">
+                        <div className="logo">
+                            <img src={Logo} alt="Logo"></img>
+                            <span style={{fontWeight: 700, fontSize: '20px', color: '#cf450e', letterSpacing: '2px', fontFamily: `'Poppins', sans-serif`}}>Fast Delivery</span>
+                        </div>
+
+                        <div className="right_nav">
+                            <ul className="nav_list">
+                                <li>logout</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </header>
