9f3c73036a771deb567bfcf2800f8a2c252ff63b
Prabesh Lama <prabesh@offshorethemes.com>
2022-11-22 08:11:07 +0545

    homepage issue fix

--- Client/src/pages/HomePage.js
+++ Client/src/pages/HomePage.js
@@ -7,14 +7,10 @@ const HomePage = () => {
   const dispatch = useDispatch()
 
   return (
-<<<<<<< HEAD:Client/src/component/homepage.js
     <>
       <div>hi i am homepage</div>
       <button className="input-button" type="submit" onClick={ () => dispatch( resetCredentials() ) }>LOGOUT</button>
     </>
-=======
-    <div>{name}</div>
->>>>>>> 6658e91da6540ddab35693bf4cbcf8a5cbcdafed:Client/src/pages/HomePage.js
   )
 
 }
