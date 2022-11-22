dcc71182c3447696788ff42ab4c27fbe154374d0
Manisha Shrestha <shresthamanisha79@gmail.com>
2022-11-22 07:21:26 +0545

    changed label

--- Client/src/component/addFood.js
+++ Client/src/component/addFood.js
@@ -7,8 +7,7 @@ const AddFood = () => {
     <div>
         <label>Food Type <input type= 'text' placeholder='Type your food type'></input></label><br/>
         <label>Restaurant <input type= 'text' placeholder='Restaurant Name'></input></label><br/>
-        <label>Preferance <select><option>Veg</option><option>NonVeg</option></select></label>
-        <button type= 'submit'>Submit</button>
+        <label>Food Category <select><option>Veg</option><option>NonVeg</option></select></label>
     </div>
   )
 }
