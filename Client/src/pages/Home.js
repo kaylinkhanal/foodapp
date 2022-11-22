ece0c76368de867fbbce4866f00dd2d0c23e96fc
Ambica <ambicana5@gmail.com>
2022-11-22 07:45:22 +0545

    added home page

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import Header from '../component/header/header'
+// import { Tabs } from 'antd';
+// import AddRestro from './AddRestro';
+// import AddFood from './AddFood';
+import { useSelector } from 'react-redux'
+import Userimage from '../images/dummy.svg'
+import {Link} from 'react-router-dom'
 
 const Home = ()=>{
+    const { name } = useSelector(state => state.users)
+    // const items = [
+    //     { label: 'Restaurant', key: 'item-1', children: <AddRestro/> }, // remember to pass the key prop
+    //     { label: 'Food', key: 'item-2', children: <AddFood/> },
+    // ];
+
     return(
         <>
             <Header />
-            <div className='home_pg'>
+            <div id='home_pg'>
                 <div className='container'>
                     <div className='main_content'>
-                        <h1>Welcome to Food Delivery App</h1>
+                    <div className='user_info'>
+                        <img src={Userimage} alt='user'/>
+                        <span> Hi {name} Welcome</span>
+                    </div>
+
+                    {/* <Tabs items={items} /> */}
+                    <div className='btn_grp'>
+                        <ul className="home_items">
+                            <li>
+                                <Link to="/admin/restro">Add Restaurant</Link>
+                            </li>
+                            <li>
+                                <Link to="/admin/food">Add Food</Link>
+                            </li>
+                        </ul>
+                    </div>
                     </div>
                 </div>
             </div>
