02dc482421ddfac085bac6e50a960c57942bffe2
medanieldark <medanieldark@gmail.com>
2022-11-22 07:41:39 +0545

    restaurant feature added

--- Client/src/pages/Auth/Register.js
+++ Client/src/pages/Auth/Register.js
@@ -1,106 +1,121 @@
-import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import { faRegistered } from '@fortawesome/free-solid-svg-icons';
- // We don't have to create the state for formik
- //Yup is use for validating the frontend filed data
- import '../../index.css'
+import FormText from '../../component/formText';
+import Image from '../../images/delivery_girl.svg'
+import { useNavigate, Link } from "react-router-dom";
+import { message } from 'antd';
+import 'antd/dist/antd.min.css';
+//import ShowHidePassword from '../../component/showHidePassword';
 
+const Register = () => {
+	const navigate = useNavigate()
+	const saveParticipants = async (values) => {
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				name: values.name,
+				phoneNumber: values.phoneNumber,
+				address: values.address,
+				email: values.email,
+				role: values.role,
+				password: values.password,
+				confirmPassword: values.confirmPassword
+			})
+		};
+		const response = await fetch('http://localhost:4000/register', requestOptions);
+		const data = await response.json();
 
- //use the yup module to create a schema for validation
- const usersSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-    address: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-    phoneNumber: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
+		if (data) {
+			console.log(data)
+			navigate('/')
+			message.success("User has been registered")
+		}
+	}
 
-  password: Yup
-  .string()
-  .required('Required')
-  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,}$/,
-  "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"),
-  confirmPassword: Yup
-  .string()
-  .required('Required')
-  .oneOf([Yup.ref('password'),null],'password must match')
-  // .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,}$/,
-  // "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-  // ),
-});
+	const passwordRule = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
 
- export const Register = () => (
-   <div className='divForm'>
-     <h1>Signup</h1>
-      {/* Wrapping form inside formik tag and passing our schema to validationSchema prop */}
-     <Formik
-       initialValues={{
-         firstName: '',
-         lastName: '',
-         address:'',
-         phoneNumber:'',
-         email: '',
-         password:'',
-         confirmPassword:''
-       }}
-       validationSchema={usersSchema}
-       onSubmit={values => {
-         // same shape as initial values
-         console.log(values);
-         const requestOptions={
-            method:'POST',
-            body:JSON.stringify(values),
-            headers:{'Content-Type':'application/json'},
-          }
-          fetch('http://localhost:4000/register', requestOptions)//this register should be from Model in line no 14
-       }}
-     >
-       {({ errors, touched }) => (
-         <Form >
-           <Field type='text' name="firstName" placeholder='First Name'/>
+	const SignupSchema = Yup.object().shape({
+		name: Yup.string()
+			.required('Required'),
+		phoneNumber: Yup.string().required('Required'),
 
-           {/* If validation is not passed show errors */}
-           {errors.firstName && touched.firstName ? (
-             <div>{errors.firstName}</div>
-           ) : null}
+		email: Yup.string().email('Invalid email').required('Required'),
 
-           <Field  type='text' name="lastName" placeholder='Last Name'/>
-           {errors.lastName && touched.lastName ? (
-             <div>{errors.lastName}</div>
-           ) : null}
+		password: Yup.string()
+			.required('Required')
+			.min(6)
+			.matches(passwordRule, { message: 'Please create a stronger password' }),
 
-           <Field type="text" name="address"  placeholder='Address'/>
-           {errors.address && touched.address ? <div>{errors.address}</div> : null}
+		confirmPassword: Yup.string()
+			.oneOf([Yup.ref('password'), null], 'Passwords doesnt match'),
+	});
 
-           <Field type="text" name="phoneNumber"  placeholder='Phone Number'/>
-           {errors.phoneNumber && touched.phoneNumber ? <div>{errors.phoneNumber}</div> : null}
+	return (
+		<div className="section_bg">
+			<div className="form_section" id="register">
+				<div className='info_text'>
+					<FormText image={Image} />
+				</div>
 
-           <Field type="email" name="email"  placeholder='Email'/>
-           {errors.email && touched.email ? <div>{errors.email}</div> : null}
+				<div className='form_content'>
+					<h2 className='pg_title'>Create Your Account</h2>
 
-           <Field type="password" name="password"  placeholder='Password'/>
-           {errors.password && touched.password ? <div>{errors.password}</div> : null}
+					<div className="register">
+						<Formik
+							initialValues={{
+								name: '',
+								phoneNumber: '',
+								address: '',
+								email: '',
+								role: '',
+								password: '',
+								confirmPassword: ''
+							}}
+							validationSchema={SignupSchema}
+							onSubmit={values => {
+								saveParticipants(values)
+								// same shape as initial values
+								// console.log('clicked');
+							}}
+						>
+							{({ errors, touched, values, handleChange, handleBlur, handleSubmit }) => (
+								<Form onSubmit={handleSubmit}>
+									<Field name="name" placeholder="Enter Name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
+									{errors.name && touched.name ? (<div className="error">{errors.name}</div>) : null}
 
-           <Field type="password" name="confirmPassword"  placeholder='Confirm Password'/>
-           {errors.confirmPassword && touched.confirmPassword ? <div>{errors.confirmPassword}</div> : null}
+									<Field name="phoneNumber" placeholder="Enter Phone No." value={values.phoneNumber} onChange={handleChange} onBlur={handleBlur} />
+									{errors.phoneNumber && touched.phoneNumber ? (<div className="error">{errors.phoneNumber}</div>) : null}
 
-           <input type="submit" value="submit"/>
-         </Form>
-       )}
-     </Formik>
-   </div>
- );
+									<Field name="address" placeholder="Enter Address" value={values.address} onChange={handleChange} onBlur={handleBlur} />
+									{errors.address && touched.address ? <div className="error">{errors.address}</div> : null}
 
- export default Register;
+									<Field name="email" type="email" placeholder="Enter Email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+									{errors.email && touched.email ? <div className="error">{errors.email}</div> : null}
+
+									<select name="role" value={values.role} onChange={handleChange} onBlur={handleBlur}>
+										<option value="" disabled="disabled" label="Select a Role"></option>
+										<option value="user" label="User">User</option>
+										<option value="rider" label="Rider">Rider</option>
+									</select>
+									{errors.role && touched.role ? <div className="error">{errors.role}</div> : null}
+
+									<Field name="password" type="password" placeholder="Enter Password" value={values.password} onChange={handleChange} onBlur={handleBlur} ></Field>
+									{errors.password && touched.password ? <div className="error">{errors.password}</div> : null}
+
+									<Field name="confirmPassword" type="password" placeholder="Confirm Password" value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur} />
+									{errors.confirmPassword && touched.confirmPassword ? <div className="error">{errors.confirmPassword}</div> : null}
+
+									<button type="submit">Submit</button>
+								</Form>
+							)}
+						</Formik>
+						<p style={{ color: '#fff', marginTop: '10px' }}>Already have an account? Please <Link to="/">Login</Link> to continue</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Register
