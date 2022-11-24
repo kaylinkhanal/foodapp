7bdf8d6d3dc3240c74cddcd956c74efe3aeb0005
Prabesh Lama <prabesh@offshorethemes.com>
2022-11-24 17:53:06 +0545

    star rating fraction add

--- Client/src/pages/HomePage.js
+++ Client/src/pages/HomePage.js
@@ -35,7 +35,7 @@ const HomePage = () => {
             <>
               <div className='foodapp-res-card-item'>
                 <div className=''><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOxDnWRNODj8srZmAnpGvF-rpzur_DD8UyixQwiy1wqxXfgUZrFp3MF5Xe7tX0UfX44WA&usqp=CAU" /></div>
-                <Rating initialValue={ item.rating } readonly />
+                <Rating initialValue={ item.rating } allowFraction={ true } readonly />
                 <div>{ item.name }</div>
                 <div>{ item.category }</div>
                 <div>{ item.location }</div>
