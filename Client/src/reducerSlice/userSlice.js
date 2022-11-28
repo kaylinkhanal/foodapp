a0fce6d753e8c6a80afa602fe4c805c6afc7bbbd
Ambica <ambicana5@gmail.com>
2022-11-28 13:46:54 +0545

    updated userSlice

--- Client/src/reducerSlice/userSlice.js
+++ Client/src/reducerSlice/userSlice.js
@@ -4,6 +4,7 @@ export const initialState = {
     name: '',
     phoneNumber: '',
     token: '',
+    role: '',
 }
 
 const userSlice = createSlice({
@@ -13,6 +14,8 @@ const userSlice = createSlice({
         setCredentials: (state, actions)=>{
             state.name = actions.payload.name
             state.phoneNumber = actions.payload.phoneNumber
+            state.token = actions.payload.token
+            state.role = actions.payload.role
         }
     }
 })
