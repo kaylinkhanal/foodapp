a70d8872201e0de6cee3af8fbbb96bb3014a7f4a
medanieldark <medanieldark@gmail.com>
2022-11-29 08:02:36 +0545

    changes made locally

--- Server/src/Controller/restaurantRouter.js
+++ Server/src/Controller/restaurantRouter.js
@@ -1,30 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-const Restaurant = require('../Model/restaurantsSchema')
-const router = express.Router();
-
-// post request for register the restaurant
-router.post("/", async (req, res) => {
-    try{
-        console.log(req.body)
-        const registeredRestaurant = await Restaurant.create(req.body)
-       res.send({
-        message: 'Restaurant Registered'
-       })
-    }catch(error){
-        console.log(error)
-    }
-});
-
-
-// view restaurants
-router.get("/", async (req, res) => {
-    try{
-    const resturantsList = await Restaurant.find()
-       res.send({
-        resturantsList: resturantsList
-       })
-=======
 const Restaurant = require('../Model/restaurantSchema')
 const router = express.Router();
 
@@ -53,14 +27,9 @@ router.get("/", async (req, res) => {
         res.json({
             restaurantList : restaurantList
         })
->>>>>>> 58f38a43656896efa1726a6f237b28049c88c054
     }catch(error){
         console.log(error)
     }
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 58f38a43656896efa1726a6f237b28049c88c054
